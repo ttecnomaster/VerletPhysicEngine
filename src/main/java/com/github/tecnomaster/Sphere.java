b36a31812c515d55c086a30e87398e538abf24bb
tecno-master <florian.koenig321@outlook.com>
2024-08-02 21:43:57 +0200

    documented VerletSphere and its Interfaces

--- src/main/java/com/github/tecnomaster/Sphere.java
+++ src/main/java/com/github/tecnomaster/Sphere.java
@@ -1,16 +1,81 @@
 package com.github.tecnomaster;
 
 public interface Sphere {
+
+    /**
+     * Updates the Spheres position according to the verlet formula.
+     * @param dt the amount of "time" to step forwards
+     */
     void updatePosition(float dt);
+
+    /**
+     * Accelerates the Sphere adding momentum to it.
+     * @param x The X Momentum
+     * @param y The Y Momentum
+     */
     void accelerate(double x, double y);
+
+    /**
+     * Sets the X Position.
+     * @param x The value to set
+     */
     void setX(double x);
+
+    /**
+     * Sets the y Position.
+     * @param y The value to set
+     */
     void setY(double y);
+
+    /**
+     * Sets the Old X Position.
+     * will change how the sphere calculates its momentum
+     * @param x The value to set
+     */
     void setOldX(double x);
+
+    /**
+     * Sets the Old Y Position.
+     * will change how the sphere calculates its momentum
+     * @param y The value to set
+     */
     void setOldY(double y);
+
+    /**
+     * Gets the X Position.
+     * @return the value that is returned
+     */
     double getX();
+
+    /**
+     * Gets the Y Position.
+     * @return the value that is returned
+     */
     double getY();
+
+    /**
+     * Gets the Old X Position.
+     * @return the value that is returned
+     */
     double getOldX();
+
+    /**
+     * Gets the Old Y Position.
+     * @return the value that is returned
+     */
     double getOldY();
+
+    /**
+     * Gets the radius of the Sphere
+     * @return the value that is returned
+     */
     float getRadius();
+
+    /**
+     * Gets the weight of the Sphere.
+     * In normal scenarios this only depends on the radius, but the weight can be modified when implementing a different Sphere
+     * @return the value that is returned
+     */
     float getWeight();
+
 }
--- src/main/java/com/github/tecnomaster/implementation/VerletSphere.java
+++ src/main/java/com/github/tecnomaster/implementation/VerletSphere.java
@@ -1,12 +1,18 @@
 package com.github.tecnomaster.implementation;
 
 import com.github.tecnomaster.Sphere;
+import com.github.tecnomaster.Verlet;
 
 public class VerletSphere implements Sphere {
     private double ax,ay;
     private double lx,ly;
     private double x,y;
     private float radius;
+
+    /**
+     * Cannot be instanced! <br>
+     * Use {@link Verlet#createSphere(double, double, float)} instead!
+     */
     protected VerletSphere() {}
 
     public void setAttributes(double x, double y, float radius) {
@@ -19,6 +25,10 @@ public class VerletSphere implements Sphere {
         this.radius = radius;
     }
 
+    /**
+     * Updates the Spheres position according to the verlet formula.
+     * @param dt the amount of "time" to step forwards
+     */
     @Override
     public void updatePosition(float dt) {
 
@@ -40,57 +50,105 @@ public class VerletSphere implements Sphere {
 
     }
 
+    /**
+     * Accelerates the Sphere adding momentum to it.
+     * @param x The X Momentum
+     * @param y The Y Momentum
+     */
     @Override
     public void accelerate(double x, double y) {
         ax += x;
         ay += y;
     }
 
+    /**
+     * Sets the X Position.
+     * @param x The value to set
+     */
     @Override
     public void setX(double x) {
         this.x = x;
     }
 
+    /**
+     * Sets the y Position.
+     * @param y The value to set
+     */
     @Override
     public void setY(double y) {
         this.y = y;
     }
 
+    /**
+     * Sets the Old X Position.
+     * will change how the sphere calculates its momentum
+     * @param x The value to set
+     */
     @Override
     public void setOldX(double x) {
         this.lx = x;
     }
 
+    /**
+     * Sets the Old Y Position.
+     * will change how the sphere calculates its momentum
+     * @param y The value to set
+     */
     @Override
     public void setOldY(double y) {
         this.ly = y;
     }
 
+    /**
+     * Gets the X Position.
+     * @return the value that is returned
+     */
     @Override
     public double getX() {
         return x;
     }
 
+    /**
+     * Gets the Y Position.
+     * @return the value that is returned
+     */
     @Override
     public double getY() {
         return y;
     }
 
+    /**
+     * Gets the Old X Position.
+     * @return the value that is returned
+     */
     @Override
     public double getOldX() {
         return lx;
     }
 
+    /**
+     * Gets the Old Y Position.
+     * @return the value that is returned
+     */
     @Override
     public double getOldY() {
         return ly;
     }
 
+    /**
+     * Gets the radius of the Sphere
+     * @return the value that is returned
+     */
     @Override
     public float getRadius() {
         return radius;
     }
 
+    /**
+     * Gets the weight of the Sphere.
+     * In normal scenarios this only depends on the radius, but the weight can be modified when implementing a different Sphere
+     * @return the value that is returned
+     */
     @Override
     public float getWeight() {
         return getRadius();
