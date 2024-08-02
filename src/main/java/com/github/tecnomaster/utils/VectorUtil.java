1d57637078760fd241aaf9c85644b91d015c3f95
tecno-master <florian.koenig321@outlook.com>
2024-08-02 20:50:26 +0200

    documented VectorUtil.java

--- src/main/java/com/github/tecnomaster/utils/VectorUtil.java
+++ src/main/java/com/github/tecnomaster/utils/VectorUtil.java
@@ -1,6 +1,13 @@
 package com.github.tecnomaster.utils;
 
 public class VectorUtil {
+    /**
+     * Calculates the length of a vector. Uses the pythagorean theorem in order to calculate the length
+     *
+     * @param x The first part of the vector
+     * @param y The second part of the vector
+     * @return the length/magnitude of the vector
+     */
     public static double length(double x, double y) {
         return Math.sqrt(x*x+y*y);
     }
