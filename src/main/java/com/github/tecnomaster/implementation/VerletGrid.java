b8de38238a38905958d9943990a279cb0bba8379
tecno-master <florian.koenig321@outlook.com>
2024-08-02 20:34:55 +0200

    downgraded visibility of VerletGrid

--- src/main/java/com/github/tecnomaster/implementation/VerletGrid.java
+++ src/main/java/com/github/tecnomaster/implementation/VerletGrid.java
@@ -10,10 +10,10 @@ public class VerletGrid {
     private double x,y,width,height;
     private float radius;
     private Cell[][] cells;
-    public VerletGrid(double x, double y, float radius) {
+    VerletGrid(double x, double y, float radius) {
         this(-x,-y,x*2,y*2,radius);
     }
-    public VerletGrid(double x, double y, double width, double height, float radius) {
+    VerletGrid(double x, double y, double width, double height, float radius) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -31,7 +31,7 @@ public class VerletGrid {
         }
     }
 
-    public void assignCells(VerletContainer container) {
+    void assignCells(VerletContainer container) {
         invokeCells(Cell::clear);
         container.invokeSpheres((sphere -> {
             double px = (sphere.getX()-x) / radius;
@@ -44,7 +44,7 @@ public class VerletGrid {
         return x >= 0 && x < this.cells.length && y >= 0 && y < this.cells[0].length;
     }
 
-    public void invokeCellsSkipBordersAndNeighborCell(Cell2Runnable runnable) {
+    void invokeCellsSkipBordersAndNeighborCell(Cell2Runnable runnable) {
         for(int i = 1; i < cells.length-1; i++) {
             for(int j = 1; j < cells[i].length-1; j++) {
                 Cell oCell = cells[i][j];
@@ -55,7 +55,7 @@ public class VerletGrid {
         }
     }
 
-    public void invokeNeighborCells(int x, int y, CellRunnable runnable) {
+    void invokeNeighborCells(int x, int y, CellRunnable runnable) {
         for(int dy = -1; dy <= 1; dy++) {
             for(int dx = -1; dx <= 1; dx++) {
                 runnable.run(cells[x+dx][y+dy]);
@@ -63,22 +63,22 @@ public class VerletGrid {
         }
     }
 
-    public void invokeCells(CellRunnable runnable) {
+    void invokeCells(CellRunnable runnable) {
         for(Cell[] cells : cells) {
             for(Cell cell : cells) {
                 runnable.run(cell);
             }
         }
     }
-    public interface CellRunnable {
+    interface CellRunnable {
         void run(Cell cell);
     }
 
-    public interface Cell2Runnable {
+    interface Cell2Runnable {
         void run(Cell cell_1, Cell cell_2);
     }
 
-    public static class Cell {
+    static class Cell {
         private final Set<Sphere> spheres = new HashSet<>();
         private void clear() {
             spheres.clear();
