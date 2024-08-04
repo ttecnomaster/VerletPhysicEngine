72f2d9710ec9587bd04a0afe44b5c76fa8e92bfa
tecno-master <florian.koenig321@outlook.com>
2024-08-04 10:41:09 +0200

    added documentation to the functional interfaces

--- src/main/java/com/github/tecnomaster/implementation/ConstraintRunnable.java
+++ src/main/java/com/github/tecnomaster/implementation/ConstraintRunnable.java
@@ -2,6 +2,21 @@ package com.github.tecnomaster.implementation;
 
 import com.github.tecnomaster.Constraint;
 
+/**
+ * This interface is used for running a Runnable with a Constraint as a parameter.
+ * This is primarily used by the {@link com.github.tecnomaster.Scene#invokeConstraints(ConstraintRunnable)} method.
+ * As a functional interface this should be used with lambda only.
+ *
+ * @author tecno-master
+ * @see com.github.tecnomaster.Scene
+ * @version 1.0.0
+ */
+@FunctionalInterface
 public interface ConstraintRunnable {
+    /**
+     * When called passes a Constraint parameter. run executes whatever the interface was created for
+     *
+     * @param constraint The Constraint that gets called on the run method
+     */
     void run(Constraint constraint);
 }
--- src/main/java/com/github/tecnomaster/implementation/SphereRunnable.java
+++ src/main/java/com/github/tecnomaster/implementation/SphereRunnable.java
@@ -2,6 +2,21 @@ package com.github.tecnomaster.implementation;
 
 import com.github.tecnomaster.Sphere;
 
+/**
+ * This interface is used for running a Runnable with a Sphere as a parameter.
+ * This is primarily used by the {@link com.github.tecnomaster.VerletContainer#invokeSpheres(SphereRunnable)} method.
+ * As a functional interface this should be used with lambda only.
+ *
+ * @author tecno-master
+ * @see com.github.tecnomaster.VerletContainer
+ * @version 1.0.0
+ */
+@FunctionalInterface
 public interface SphereRunnable {
+    /**
+     * When called passes a Sphere parameter. run executes whatever the interface was created for
+     *
+     * @param sphere The Sphere that gets called on the run method
+     */
     void run(Sphere sphere);
 }
