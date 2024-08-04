94204dea60504445372f08f5aa60094a53b752a9
tecno-master <florian.koenig321@outlook.com>
2024-08-04 10:58:05 +0200

    added documentation to the Constraint interfaces

--- src/main/java/com/github/tecnomaster/Constraint.java
+++ src/main/java/com/github/tecnomaster/Constraint.java
@@ -2,7 +2,15 @@ package com.github.tecnomaster;
 
 /**
  * A Constraint is used to modify the behaviour of Spheres in a Scene.
- * Every Constraint needs to be either a SceneConstraint ora StaticConstraint in order to be applied in a Scene.
+ * Every Constraint needs to be either a {@link SceneConstraint} or a {@link StaticConstraint} in order to be applied to a Scene.
+ * A Constraint usually has an apply method.
+ * For the {@link SceneConstraint} it is {@link SceneConstraint#apply(Sphere)}.
+ * For the {@link StaticConstraint} it is {@link StaticConstraint#apply()}.
+ *
+ * @author tecno-master
+ * @see SceneConstraint
+ * @see StaticConstraint
+ * @version 1.0.0
  */
 public interface Constraint {
 }
