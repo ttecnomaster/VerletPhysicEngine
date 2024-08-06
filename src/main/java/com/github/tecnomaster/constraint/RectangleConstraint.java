b396b59cab16222371fa9c130cc9ff1796319463
Florian König <florian.koenig@siemens.com>
2024-08-06 10:41:15 +0200

    Implemented RectangleConstraint

---
+++ src/main/java/com/github/tecnomaster/constraint/RectangleConstraint.java
@@ -0,0 +1,24 @@
+package com.github.tecnomaster.constraint;
+
+import com.github.tecnomaster.SceneConstraint;
+import com.github.tecnomaster.Sphere;
+
+public class RectangleConstraint implements SceneConstraint {
+
+    private final BorderConstraint top, bottom, right, left;
+
+    public RectangleConstraint(double x, double y, double width, double height) {
+        top = new BorderConstraint(BorderConstraint.Type.TOP, y + height);
+        bottom = new BorderConstraint(BorderConstraint.Type.BOTTOM, y);
+        right = new BorderConstraint(BorderConstraint.Type.RIGHT, x + width);
+        left = new BorderConstraint(BorderConstraint.Type.LEFT, x);
+    }
+
+    @Override
+    public void apply(Sphere sphere) {
+        top.apply(sphere);
+        bottom.apply(sphere);
+        right.apply(sphere);
+        left.apply(sphere);
+    }
+}
